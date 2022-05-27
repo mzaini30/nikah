b3b284952a852b0401f2cf864275ed38c9a41ece
Zen <muhzaini30@gmail.com>
2022-05-28 06:48:31 +0800

    Oke

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
-<template>
-	<div class="prose p-3">
-		<h1>Assalamualaikum</h1>
-		<p>Welcome to this Vue template, cooked by Zen.</p>
-	</div>
-</template>
+<script setup="">
+	import {useRouter} from 'vue-router'
+
+	const {replace} = useRouter()
+
+	replace('/tes')
+</script>
---
+++ src/pages/lisensi.md
@@ -0,0 +1,5 @@
+# Lisensi
+
+Flower: <https://www.vecteezy.com/vector-art/622224-hand-drawn-spring-flower-border-background-vector-illustration>
+
+Pasangan yang menikah: <https://id.pinterest.com/pin/428123508330155386/>
